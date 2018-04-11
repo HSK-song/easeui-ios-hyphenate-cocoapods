5f9af77dca5fc892a9daaeb37543308e79d732c8
Song Zhou <zhousong1993@gmail.com>
2018-04-11 10:14:20 +0800

    localization conversation list right

--- EaseUI/EMUIKit/ViewController/EaseConversationListViewController.m
+++ EaseUI/EMUIKit/ViewController/EaseConversationListViewController.m
@@ -18,6 +18,7 @@
 #import "EaseMessageViewController.h"
 #import "NSDate+Category.h"
 #import "EaseLocalDefine.h"
+#import "EaseBundle.h"
 
 @interface EaseConversationListViewController ()
 
@@ -151,13 +152,13 @@
 - (id)setupCellEditActions:(NSIndexPath *)aIndexPath
 {
     if ([UIDevice currentDevice].systemVersion.floatValue < 11.0) {
-        UITableViewRowAction *deleteAction = [UITableViewRowAction rowActionWithStyle:UITableViewRowActionStyleDefault title:NSLocalizedString(@"delete",@"Delete") handler:^(UITableViewRowAction * _Nonnull action, NSIndexPath * _Nonnull indexPath) {
+        UITableViewRowAction *deleteAction = [UITableViewRowAction rowActionWithStyle:UITableViewRowActionStyleDefault title:[EaseBundle localizedStringForKey:@"delete"] handler:^(UITableViewRowAction * _Nonnull action, NSIndexPath * _Nonnull indexPath) {
             [self deleteCellAction:indexPath];
         }];
         deleteAction.backgroundColor = [UIColor redColor];
         return @[deleteAction];
     } else {
-        UIContextualAction *deleteAction = [UIContextualAction contextualActionWithStyle:UIContextualActionStyleDestructive title:NSLocalizedString(@"delete",@"Delete") handler:^(UIContextualAction * _Nonnull action, __kindof UIView * _Nonnull sourceView, void (^ _Nonnull completionHandler)(BOOL)) {
+        UIContextualAction *deleteAction = [UIContextualAction contextualActionWithStyle:UIContextualActionStyleDestructive title:[EaseBundle localizedStringForKey:@"delete"] handler:^(UIContextualAction * _Nonnull action, __kindof UIView * _Nonnull sourceView, void (^ _Nonnull completionHandler)(BOOL)) {
             [self deleteCellAction:aIndexPath];
         }];
         deleteAction.backgroundColor = [UIColor redColor];
